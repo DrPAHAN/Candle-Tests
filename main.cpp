6ff5fb151f6951a9547ce3ee71e360aa06e17685
DrPAHAN <mironenko.an7661@gmail.com>
2026-03-16 16:40:00 +0700

    добавлены тесты из пункта 2.2 (contains)

--- main.cpp
+++ main.cpp
@@ -23,6 +23,26 @@ TEST(CandleTest, BodyContains_DojiBoundary) {
   EXPECT_FALSE(c.body_contains(100.1));
 }
 
+TEST(CandleTest, Contains_InsideFullRange) {
+  Candle c(100.0, 110.0, 90.0, 105.0);
+  EXPECT_TRUE(c.contains(95.0));
+  EXPECT_TRUE(c.contains(90.0));
+  EXPECT_TRUE(c.contains(110.0));
+}
+
+TEST(CandleTest, Contains_Boundary) {
+  Candle c(100.0, 110.0, 90.0, 105.0);
+  EXPECT_TRUE(c.contains(90.0));
+  EXPECT_TRUE(c.contains(110.0));
+  EXPECT_FALSE(c.contains(89.9));
+}
+
+TEST(CandleTest, Contains_Outside) {
+  Candle c(100.0, 110.0, 90.0, 105.0);
+  EXPECT_FALSE(c.contains(111.0));
+  EXPECT_FALSE(c.contains(89.0));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
